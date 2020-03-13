e74d8036183ca5944d14fc5d97bd8a69106da1ca
yanmulin <yans1996@outlook.com>
2020-03-13 11:15:43 +0800

    add ubuntu setup script

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 dotfiles: FORCE
 	ln -sf `pwd`/dotfiles/vim ~/.vim
 
+ubuntu: FORCE
+	sudo ./setup/ubuntu.sh
+
 sync:
 	git pull
 	git push
---
+++ setup/ubuntu.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ -z "$SUDO_USER" ]; then
+    # if the user didn't call us with sudo, re-execute
+    exec sudo $0 "$@"
+fi
+
+### update sources and get add-apt-repository
+apt-get update
+apt-get -y install software-properties-common
+
+### add the extended source repos
+add-apt-repository multiverse
+add-apt-repository universe
+add-apt-repository restricted
+
+### make sure we're totally up-to-date now
+apt-get update
+apt-get -y dist-upgrade
+
+### install the software we need for the VM and build env
+apt-get -y install build-essential g++ gcc cmake automake pkg-config \
+		   libtool libtool-bin git iptables net-tools coreutils \
+		   netcat-openbsd curl
+
