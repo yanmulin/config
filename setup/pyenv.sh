c000f2f58927d227370ddce853853d94957640d5
yanmulin <yans1996@outlook.com>
2020-05-14 22:05:43 +0800

    update pyenv.sh

--- setup/pyenv.sh
+++ setup/pyenv.sh
@@ -10,16 +10,20 @@ case `basename $SHELL` in
     echo '# config pyenv' >> ~/.bash_profile
     echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
     echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
-    echo 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bash_profile
+    echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bash_profile
     ;;
   zsh)
+    echo >> ~/.zshrc
+    echo '# config pyenv' >> ~/.zshrc
     echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
     echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
     echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
     ;;
   fish)
-    fish -c "set -Ux PYENV_ROOT $HOME/.pyenv"
-    fish -c "set -Ux fish_user_paths $PYENV_ROOT/bin $fish_user_paths"
+    fish -c 'echo >> ~/.$__fish_config_dir/config.fish'
+    fish -c 'echo "# config pyenv" >> ~/.$__fish_config_dir/config.fish'
+    fish -c 'set -Ux PYENV_ROOT $HOME/.pyenv'
+    fish -c 'set -Ux fish_user_paths $PYENV_ROOT/bin $fish_user_paths'
     fish -c 'echo -e "if command -v pyenv 1>/dev/null 2>&1; then\n\tpyenv init - | source\nend" >> $__fish_config_dir/config.fish'
     ;;
   *) echo 'Unknown shell: ' $shell; exit -1;;
